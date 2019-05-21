5bb3db4d5d37327239289d419b488d3bdd857776
Ellison Rio <rio.ellison@gmail.com>
2019-05-21 19:06:31 -0400

    set up grid columns for playing area

---
+++ script.js
@@ -0,0 +1,29 @@
+var phraseKey = [
+    "conference room, five minutes",
+    "that's what she said",
+    "pretzel day",
+    "i declare bankruptcy",
+    "bears. beets. battlestar galactica",
+    "did i stutter",
+    "diversity day",
+    "the dundies",
+    "save bandit",
+    "keep it simple stupid",
+    "it is your birthday",
+    "party planning committee",
+    "the finer things club",
+    "flonkerton",
+    "threat level midnight",
+    "goodbye toby",
+    "lanch party",
+    "ryan started the fire",
+    "oaky afterbirth",
+    "scott's tots",
+    "michael scott paper company",
+    "dinkin flicka",
+    "here comes treble",
+    "princess unicorn",
+    "vance refrigeration"
+];
+
+console.log(phraseKey)
