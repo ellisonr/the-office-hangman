af48c62c3c221b1a9fffe0ac30a1d76694ad085a
Ellison Rio <rio.ellison@gmail.com>
2019-05-22 14:00:09 -0400

    got the individual letters to print to the page, set display to none in css to hide letters

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var phraseKey = [
     "that's what she said",
     "pretzel day",
     "i declare bankruptcy",
-    "bears. beets. battlestar galactica",
+    "bears beets battlestar galactica",
     "did i stutter",
     "diversity day",
     "the dundies",
@@ -41,21 +41,33 @@ randomPhrase()
 //create a function that will split up the random phrase
 //provided into separate letters
 const separatedPhrase = phraseInPlay.split("");
-console.log(separatedPhrase)
+// console.log(separatedPhrase)
 
 
 //create a function that will display the letters in the
 //playing area, but make them hidden at first
 
 //----------------
-//for loop that creates a space in the "playing area" for
-//each letter in the separatedPhrase
+//create for loop that creates a space in the "playing area" for each letter in the separatedPhrase
 for (var i=0; i<separatedPhrase.length; i++){
-    let placeholderForLetter = document.createElement("div")
+    let placeholderForLetter = document.createElement("div");
     placeholderForLetter.className = "spaceForLetter";
-    document.querySelector(".blank-tiles").appendChild(placeholderForLetter)
-}
+    document.querySelector(".blank-tiles").appendChild(placeholderForLetter);
+    placeholderForLetter.style.backgroundColor = "rgb(0,193,239)"; //applying color to test its covered area
 // ^for every character in the random phrase, create a div element. this div element
 // will have a className of "spaceForletter". target the div with class "blank-tiles"
 // and append that with the div that was just created
 //----------------
+    
+//the below should create an span element within each placholder div from above
+//then I'll use innerHTML to stick the individual letters into each span
+//append the value of the span into the given placeholder div
+    displayedLetter = document.createElement("span");
+    displayedLetter.className = "displayedLetter";
+    displayedLetter.innerHTML = separatedPhrase[i];
+    placeholderForLetter.appendChild(displayedLetter)
+    displayedLetter.style.color = "black";
+    // console.log(displayedLetter)
+}
+
+//on clicking a letter, if it matches, turn off that letter's "display: none"
